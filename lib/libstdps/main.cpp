1fe91b464a0135d9b731bc6d43003fd27ed698ec
Myriad-Dreamin <camiyoru@gmail.com>
2020-06-23 03:53:48 +0800

    add llvm source files

---
+++ lib/libstdps/main.cpp
@@ -0,0 +1,18 @@
+//
+// Created by kamiyoru on 2020/5/9.
+//
+
+#include <cstdint>
+
+extern "C" {
+int32_t pascal_s_main_520d4d14();
+}
+
+int main(int argc, char **argv) {
+
+    // gc
+    // 额外的函数
+    // 给pascal-s main 传参
+
+    return pascal_s_main_520d4d14();
+}
