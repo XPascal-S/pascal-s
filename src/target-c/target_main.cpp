3f440a30eb0d5b2c53b3dfb41f143e8a04bb5299
wxl1999 <wxl1999@foxmail.com>
2020-07-01 15:26:29 +0800

    add if_else ast by hand

--- src/target-c/target_main.cpp
+++ src/target-c/target_main.cpp
@@ -6,7 +6,70 @@
 #include <target/c.h>
 #include <iostream>
 
-char testChar[6] = "hello";
+
+Program * test_if_else() {
+    auto *integer = new Keyword(KeywordType::Integer);
+    auto *intKeyWord = new ExpKeyword(integer);
+    auto *mainIdentifier = new Identifier("main");
+    auto *mainIdent = new Ident(mainIdentifier);
+    auto *promHead = new ProgramHead(intKeyWord, mainIdent, nullptr);
+
+    auto *varDecls = new VarDecls;
+    auto identList = new IdentList;
+    identList->idents.push_back(new Identifier("a"));
+    varDecls->decls.push_back(new VarDecl(identList, new TypeSpec(Type::BasicTypeSpec)));
+
+    auto *readInt = new Identifier("read_int64");
+    auto *writeChar = new Identifier("write_char");
+    auto *varA = new Variable;
+    varA->id = new Identifier("a");
+    auto *varMain = new Variable;
+    varMain->id = new Identifier("main");
+    auto mainStatementList = new StatementList;
+
+    auto *readExpList = new ExpressionList;
+    readExpList->explist.push_back(varA);
+    auto *readExp = new ExpCall(readInt, readExpList);
+    mainStatementList->statement.push_back(new ExecStatement(readExp));
+
+    auto *ifelseStatement = new IfElseStatement;
+    auto *judgeExp = new BiExp(varA, new Marker(MarkerType::LT), new ExpConstantInteger(new ConstantInteger(0)));
+    auto *judgeStat = new ExecStatement(judgeExp);
+    ifelseStatement->expression = judgeStat;
+
+    auto *ifPart = new StatementList;
+    auto *writeCharExpList1 = new ExpressionList;
+    writeCharExpList1->explist.push_back(new ExpConstantChar(new ConstantChar('<')));
+    auto *callWriteChar1 = new ExpCall(writeChar, writeCharExpList1);
+    auto *ifStat = new ExecStatement(callWriteChar1);
+    ifPart->statement.push_back(ifStat);
+    ifelseStatement->if_part = ifPart;
+
+    auto *elsePart = new StatementList;
+    auto *writeCharExpList2 = new ExpressionList;
+    writeCharExpList2->explist.push_back(new ExpConstantChar(new ConstantChar('>')));
+    auto *callWriteChar2 = new ExpCall(writeChar, writeCharExpList2);
+    auto *elseStat1 = new ExecStatement(callWriteChar2);
+    auto *writeCharExpList3 = new ExpressionList;
+    writeCharExpList3->explist.push_back(new ExpConstantChar(new ConstantChar('=')));
+    auto *callWriteChar3 = new ExpCall(writeChar, writeCharExpList3);
+    auto *elseStat2 = new ExecStatement(callWriteChar3);
+    elsePart->statement.push_back(elseStat1);
+    elsePart->statement.push_back(elseStat2);
+    ifelseStatement->else_part = elsePart;
+
+    mainStatementList->statement.push_back(ifelseStatement);
+
+    auto *returnExp = new ExpAssign(varMain, new ExpConstantInteger(new ConstantInteger(0)));
+    auto *returnStat = new ExecStatement(returnExp);
+    mainStatementList->statement.push_back(returnStat);
+
+    auto *mainCompoundStat = new CompoundStatement(mainStatementList);
+    auto *mainBody = new ProgramBody(nullptr, varDecls, nullptr, mainCompoundStat);
+
+    auto *mainProgram = new Program(promHead, mainBody);
+    return mainProgram;
+}
 
 int main()
 {
@@ -18,6 +81,7 @@ int main()
     StatementList *mainStatementList = new StatementList;
     Identifier *writeChar = new Identifier("write_char");
 
+    char testChar[6] = "hello";
     for(int i=0; i<5; i++) {
         ExpressionList *constStringList = new ExpressionList;
         ConstantChar *hchar = new ConstantChar(testChar[i]);
